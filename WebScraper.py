0944bdabb478165de4369a13d29eb93f375b3b2b
schgrz <schgrz@gmail.com>
2023-06-27 22:16:07 -0400

    Updating a few lines of code and changing some comments

--- WebScraper.py
+++ WebScraper.py
@@ -7,26 +7,28 @@ import csv
 bookinfo = []
 
 # Send a GET request to the website and parse the HTML content and iterate it through all pages
-for page_num in range(1,50):
+for page_num in range(1, 50):
     url = f"https://books.toscrape.com/catalogue/page-{page_num}.html"
     result = rq.get(url)
     website = bs(result.text, "html.parser")
-#--View it if we'd like
-#print(website.prettify())
+    # --View it if we'd like (Remove # from the line below)
+    # print(website.prettify())
 
-# Find the prices
+    # Find the prices
     prices = website.find_all(class_="price_color")
     prices = [price.text.strip("Â") for price in prices]
-# For my purposes, I'll be viewing this in Dollars to make it make sense for me, however the website is in British Pounds
+    # For my purposes, I'll be viewing this in Dollars to make it make sense for me, however the website is in British Pounds
     prices = [price.strip("Â").replace("£", "$") for price in prices]
-#print(prices)
+    # --View it if we'd like (Remove # from the line below)
+    # print(prices)
 
-# Find the titles
+    # Find the titles
     titles = website.find_all("a", title=True)
     titles = [title["title"] for title in titles]
-#print(titles)
+    # --View it if we'd like (Remove # from the line below)
+    # print(titles)
 
-# Find the ratings
+    # Find the ratings
     ratings = website.find_all("p", class_=True)
     ratings = []
     for rating in website.find_all("p", class_="star-rating"):
@@ -44,15 +46,14 @@ for page_num in range(1,50):
         else:
             rating_value = "Unknown"
         ratings.append(rating_value)
-#print(ratings)
+    # --View it if we'd like (Remove # from the line below)
+    # print(ratings)
 
-
-    page_data = list(zip(titles, prices,ratings))
+    page_data = list(zip(titles, prices, ratings))
     bookinfo.extend(page_data)
-#for title, price, rating in bookinfo:
-#    print(title,price,rating)
 
-#print(bookinfo)
+# --View it if we'd like (Remove # from the line below)
+# print(bookinfo)
 
 # Now I'll write it as a CSV file and store it for later
 output_file = "bookinfo.csv"
@@ -64,5 +65,5 @@ with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
     for row in bookinfo:
         writer.writerow(row)
 
-# This lets me know the program has completed        
+# This lets me know the program has completed
 print("Done")
